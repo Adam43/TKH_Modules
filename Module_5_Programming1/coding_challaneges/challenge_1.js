ada6ab6cd28f63d9b54ebbf2fe6ecc8d8aa9e8fa
Adam43 <kwaali_b@live.com>
2021-08-06 23:10:31 -0400

    first commit

---
+++ Module_5_Programming1/coding_challaneges/challenge_1.js
@@ -0,0 +1,2 @@
+["Westly Snipes", "Nicholas Cage", "Nasir Jones", "Sean Carter", "Sean Combs", "Michael Jordan", "Patrick Ewing"]
+
---
+++ Module_5_Programming1/coding_challaneges/challenge_3.js
@@ -0,0 +1,6 @@
+//Challenge 3
+
+"Sup bro how are you"
+['Sup', 'bro', 'how', 'are', 'you']
+['$up', 'bro', 'how', 'are', 'you']
+"$up bro how are you" 
